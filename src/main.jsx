1d02a5866d6f496a70ccd022f19a3313fe70350e
YevheniyaSolomyana <72937387+YevheniyaSolomyana@users.noreply.github.com>
2024-06-19 13:29:21 -0400

    fixing main

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.hydrateRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
