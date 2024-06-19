89536e08831ace63e84423083723028822f85ef2
YevheniyaSolomyana <72937387+YevheniyaSolomyana@users.noreply.github.com>
2024-06-19 13:34:34 -0400

    fixing main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
-const root = ReactDOM.hydrateRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.hydrateRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+const container = document.getElementById('root');
+hydrateRoot(
+  container,
   <React.StrictMode>
     <BrowserRouter>
       <App />
