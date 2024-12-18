6ddf5590f4fed88bd43591cddc467fc48f85554d
5R Photo Lab <5rphotolab@Alecs-Air.lan>
2024-12-18 18:59:41 -0500

    finished ServicesMobile started on MailInMobile

--- src/components/MailInMobile.jsx
+++ src/components/MailInMobile.jsx
@@ -1,7 +1,28 @@
+import '../styles/MailInMobile.css';
+import mailStamp from '../assets/mail-stamp.png';
+
 const MailInMobile = () => {
     return (
         <>
-            <h1>MailInMobile</h1>
+            <div className="main-container-mailin">
+                <div className="top-mailin">
+                    <div className="triangle"></div>
+                </div>
+                <div className="bottom-mailin">
+                    <img src={mailStamp} alt="Mail Stamp" />
+                    <h2>We offer mail-in development, scan, and print services!</h2>
+                    <p><b>Not in New York City?</b> We can still develop your film—including all those old disposable cameras you’ve got lying around the house!</p> <br />
+
+                    <p><b>It’s super easy!</b> Mail your film or disposables to <u>31 Washington Square West, Suite 5-RC, New York NY, 10011</u>. Please include a note
+                        with your contact information and what you would like done with your film and/or disposables. Would you like digital scans only? Do you want prints, too? Do you want your negatives back?</p> <br />
+
+                    <p><b>It’s very important</b> to protect your disposables and film in transit. Stick your film in a plastic Ziplock bag. Some people use envelopes with the bubble padding, but we think that a sturdy box with some protective padding inside, secured by solid tape, is the best way.
+                        Be sure to indicate on the box “Do not X-Ray.” There is more info on what to say about <a href='https://kodakprofessional.com/photographers/resources' target='_blank'>X-Rays here</a>.</p> <br />
+
+                    <p><b>We will email an invoice</b> when we get your order. Once payment is processed, we will go ahead and start on your lovely pictures!
+                        If you want your negatives back, or if you are ordering prints, return shipping starts at $10. </p> <br />
+                </div>
+            </div>
         </>
     )
 }
---
+++ src/components/PricesContentMobile.jsx
@@ -0,0 +1,90 @@
+import '../styles/PricesContentMobile.css';
+
+const PricesContentMobile = () => {
+    const colorData = [
+        { price: "$19.99", description: "Standard 2-Day Dev&Scan" },
+        { price: "$24.99", description: "Standard 2-Day Dev&Scan HI-RES" },
+        { price: "$23.99", description: "Standard 2-Day Dev&Print, 24-27 Exp." },
+        { price: "$25.99", description: "Standard 2-Day Dev&Print, 36 Exp." },
+    ];
+
+    const bwData = [
+        { price: "$19.99", description: "Standard 5-Day Dev&Scan" },
+        { price: "$24.99", description: "Standard 5-Day Dev&Scan HI-RES" },
+        { price: "$21.99", description: "Standard 5-Day Dev&Print, 24-27 Exp." },
+        { price: "$23.99", description: "Standard 5-Day Dev&Print, 36 Exp." },
+    ];
+
+    const printData = [
+        { price: "$07.00", description: "24-27 Exp. (4X6 Prints)" },
+        { price: "$09.00", description: "36 Exp. (4X6 Prints)" },
+        { price: "$12.00", description: "8X10 Print" },
+        { price: "$15.00", description: "8X12 Print" },
+    ];
+
+    const moreData = [
+        { price: "$29.99", description: "120 Dev&Scan" },
+        { price: "$19.99", description: "110 Dev&Scan" },
+        { price: "$19.99", description: "APS Dev&Scan" },
+        { price: "$20.00", description: "Negatives Scan per Strip" },
+        { price: "$05.00", description: "Scan per Slide/Print, 10 Minimum" },
+    ];
+
+    const addOnsData = [
+        { price: "$45.00", description: "Same-Day 35mm Color Dev&Scan" },
+        { price: "$35.00", description: "Next-Day 35mm Color Dev&Scan" },
+        { price: "$15.99", description: "Three-Day 35mm Color Dev&Scan" }
+    ];
+
+    return (
+        <>
+            <div className="prices">
+                <h2 className="prices-title">35mm Color</h2>
+                {colorData.map((item, index) => (
+                    <div key={index} className={`prices-row${index > 0 ? "-" + (index + 1) : ""}`}>
+                        <div className="prices-price">{item.price}</div>
+                        <div className="prices-description">{item.description}</div>
+                    </div>
+                ))}
+            </div>
+            <div className="prices">
+                <h2 className="prices-title">35mm B&W</h2>
+                {bwData.map((item, index) => (
+                    <div key={index} className={`prices-row${index > 0 ? "-" + (index + 1) : ""}`}>
+                        <div className="prices-price">{item.price}</div>
+                        <div className="prices-description">{item.description}</div>
+                    </div>
+                ))}
+            </div>
+            <div className="prices">
+                <h2 className="prices-title">Print Set</h2>
+                {printData.map((item, index) => (
+                    <div key={index} className={`prices-row${index > 0 ? "-" + (index + 1) : ""}`}>
+                        <div className="prices-price">{item.price}</div>
+                        <div className="prices-description">{item.description}</div>
+                    </div>
+                ))}
+            </div>
+            <div className="prices">
+                <h2 className="prices-title">More</h2>
+                {moreData.map((item, index) => (
+                    <div key={index} className={`prices-row${index > 0 ? "-" + (index + 1) : ""}`}>
+                        <div className="prices-price">{item.price}</div>
+                        <div className="prices-description">{item.description}</div>
+                    </div>
+                ))}
+            </div>
+            <div className="prices">
+                <h2 className="prices-addons-title">Add-ons</h2>
+                {addOnsData.map((item, index) => (
+                    <div key={index} className={`prices-row${index > 0 ? "-" + (index + 1) : ""}`}>
+                        <div className="prices-addons-price">{item.price}</div>
+                        <div className="prices-description">{item.description}</div>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default PricesContentMobile;
