acd0eec7155f84cf3c9db7e0448a70527b5bcfa3
5R Photo Lab <5rphotolab@Alecs-Air.lan>
2025-02-12 15:35:10 -0500

    adding error page and modifying servicesmobile

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,8 +1,16 @@
+import '../styles/ErrorPage.css'
+
 const ErrorPage = () => {
     return (
-        <>
-        <h1>Bad Page!</h1>
-        </>
+        <div className='main-container-error'>
+            {/* <div className="top-bar">
+                <h1 className="bad-page">Bad Page!</h1>
+
+            </div> */}
+            <h1 className="bad-page">404</h1>
+            <h1 className="bad-page">Not Found</h1>
+
+        </div>
     )
 }
 
