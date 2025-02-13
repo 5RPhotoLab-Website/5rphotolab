b5be0251045338bc1a54c1c09ddb53a450ea0e82
5R Photo Lab <5rphotolab@Alecs-Air.lan>
2025-02-13 15:58:59 -0500

    homepage

--- src/components/HomePageMobile.jsx
+++ src/components/HomePageMobile.jsx
@@ -1,5 +1,6 @@
 import "../styles/HomePageMobile.css"
 import filmyColorOnly from '../assets/filmy-color-only.png';
+import FiveRLOGO from '../assets/FiveRLOGO.png';
 import searchIcon from '../assets/search.png'
 import videoMapIconMobile from '../assets/videoMapIconMobile.png'
 import sixTrainIcon from '../assets/sixTrainIcon.png'
@@ -19,21 +20,33 @@ const HomePageMobile = () => {
 
     return (
         <div className="main-container-mobile">
-            <h1 className="sideNavTitleMobile">5R PHOTO LAB</h1>
+            <div className="logo-container">
+            <img className="fiverlogo" src={FiveRLOGO} alt="5R Logo" />
+
+            </div>
+            {/* <img className="fiverlogo" src={FiveRLOGO} alt="5R Logo" /> */}
+            <div className="welcome"><p>welcome to 5r photo lab!</p></div>
+
+            {/* <h1 className="sideNavTitleMobile">5R PHOTO LAB</h1> */}
             <div className="box1"></div>
             <div className="box2"></div>
-            <div className="box3"><p>HEY THERE</p></div>
-            <div className="containerMobile">
+            {/* <div className="box3"><p>HEY THERE</p></div> */}
+            <div className="startMailInBtn"><p>start your mail in order</p></div>
+            {/* <div className="containerMobile">
                 <div className="leftSideMobile">
                     <div className="box4"></div>
                     <div className="box5"><p>MY NAME IS FILMY!</p></div>
                     <div className="box6"></div>
                 </div>
                 <img className="filmyColorOnly" src={filmyColorOnly} alt="Filmy Color Logo" />
-            </div>
+            </div> */}
             <div className="box7"></div>
+            <div className="moreInfoBtn"><p>more info on mail in services</p></div>
             <div className="box8"></div>
-            <div className="box9">KEEP SCROLLING</div>
+            <div className="box9">visit us in nyc</div>
+            <div className="box1"></div>
+            <div className="box1"></div>
+
 
             <div className="top-bar-mobile">
                 <img id="searchIconMobile" src={searchIcon} alt="" />
